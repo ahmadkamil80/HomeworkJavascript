ff46bbe8b4c11dde7227c8b85f7b37f1e1b886ac
AhmadKamil <ahmadkamil80@gmail.com>
2021-08-24 10:16:10 +0700

    Membuat aplikasi conversi suhu

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,12 @@
 const Convert = require("../src/js/Convert")
 
 const convert = new Convert();
-test("convert celcius to reamur", () => {
-    expect(convert.fromCelcius(20, 20),toBe(16))
-})
+test("convert celcius to cel", () => {
+    expect(convert.fromCelcius("rea", 20)).toBe(16)
+});
+test("convert celcius to far", () => {
+    expect(convert.fromCelcius("far", 20)).toBe(68)
+});
+test("convert celcius to cel", () => {
+    expect(convert.fromCelcius("cel", 20)).toBe(20)
+});
